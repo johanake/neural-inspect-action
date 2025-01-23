a7441ca17e1ade2f3645f76d2ea63a6bba7780b7
Johan Åkesson <johan@akesson.st>
2025-01-23 20:51:06 +0100

    rollup fixed

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,18 +1,17 @@
 // See: https://rollupjs.org/introduction/
 
+import { defineConfig } from 'rollup'
+import typescript from '@rollup/plugin-typescript'
+import json from '@rollup/plugin-json'
 import commonjs from '@rollup/plugin-commonjs'
 import nodeResolve from '@rollup/plugin-node-resolve'
-import typescript from '@rollup/plugin-typescript'
 
-const config = {
+export default defineConfig({
   input: 'src/main.ts',
   output: {
-    esModule: true,
     file: 'dist/main.js',
-    format: 'es',
+    format: 'cjs',
     sourcemap: true
   },
-  plugins: [typescript(), nodeResolve(), commonjs()]
-}
-
-export default config
+  plugins: [typescript(), json(), commonjs(), nodeResolve()]
+})
