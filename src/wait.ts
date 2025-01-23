0652adff52fd05dd165395cc5532138351e86785
Johan Åkesson <johan@akesson.st>
2025-01-23 20:38:43 +0100

    added log

--- src/wait.ts
+++ src/wait.ts
@@ -1,3 +1,5 @@
+import * as core from '@actions/core'
+
 /**
  * Waits for a number of milliseconds.
  *
@@ -6,7 +8,7 @@
  */
 export async function wait(milliseconds: number): Promise<string> {
   return new Promise((resolve) => {
-    console.log('hello')
+    core.info('hello')
     if (isNaN(milliseconds)) throw new Error('milliseconds is not a number')
 
     setTimeout(() => resolve('done!'), milliseconds)
